0a6d343170303b98f212ef9ddf40aac4c3b4bedb
Ernest <earneyzxl@gmail.com>
2015-06-09 12:22:20 +0800

    add "Maximum Depth of Binary Tree"

---
+++ common/src/main/java/TreeNode.java
@@ -0,0 +1,12 @@
+/**
+ * Definition for a binary tree node.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
