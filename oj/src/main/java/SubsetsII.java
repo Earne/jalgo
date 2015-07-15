ef4840917b2b6c73b00ed9d6ad1183f7f87bcad8
Ernest <earneyzxl@gmail.com>
2015-07-15 20:53:24 +0800

    add "Subsets II"

---
+++ oj/src/main/java/SubsetsII.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+ * <p>
+ * Note:
+ * <p>
+ * Elements in a subset must be in non-descending order.
+ * The solution set must not contain duplicate subsets.
+ * <p>
+ * For example,
+ * If nums = [1,2,2], a solution is:
+ * <p>
+ * [
+ * [2],
+ * [1],
+ * [1,2,2],
+ * [2,2],
+ * [1,2],
+ * []
+ * ]
+ */
+public class SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        res.add(new ArrayList<Integer>());
+        Arrays.sort(nums);
+        helper(res, nums, new ArrayList<Integer>(), 0);
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res, int[] nums, ArrayList<Integer> curList, int index) {
+        if (index == nums.length)
+            return;
+        for (int i = index; i < nums.length; i++) {
+            // TODO refactor this
+            if (i > index && nums[i] == nums[i - 1])
+                continue;
+            curList.add(nums[i]);
+            res.add(new ArrayList<Integer>(curList));
+            helper(res, nums, curList, i + 1);
+            curList.remove(curList.size() - 1);
+        }
+    }
+}
---
+++ oj/src/test/java/SubsetsIITest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class SubsetsIITest {
+
+    @Test
+    public void testSubsetsWithDup() throws Exception {
+        SubsetsII s = new SubsetsII();
+        System.out.println(s.subsetsWithDup(new int[]{1, 2, 2}));
+    }
+}
