e5c0e02a945feb4c7d00dc39f72789bdc5406a3a
Ernest <earneyzxl@gmail.com>
2015-09-28 15:39:09 +0800

    add "Performance Log" with some ERROR

--- writtenexam/src/main/java/PerformanceLog.java
+++ writtenexam/src/main/java/PerformanceLog.java
@@ -8,6 +8,10 @@ import java.util.*;
 
 class PLNode {
     static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+    static {
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+    }
     String name;
     Date stamp;
     String action;
@@ -23,19 +27,19 @@ public class PerformanceLog {
     public static void main(String[] args) throws ParseException {
         Scanner cin = new Scanner(System.in);
         int N = Integer.parseInt(cin.nextLine());
-//        List<String> res = new ArrayList<>();
-        Map<String, Long> res = new LinkedHashMap<>();
+        Map<String, String> res = new LinkedHashMap<>();
         Stack<PLNode> stack = new Stack<>();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        boolean isError = false;
         Date curTime = null;
         for (int i = 0; i < N; i++) {
             String[] curStr = cin.nextLine().split(" ");
             PLNode cur = new PLNode(curStr[0], curStr[1], curStr[2]);
             if (curTime == null)
                 curTime = sdf.parse(curStr[1]);
-
-            if (curTime.compareTo(cur.stamp) > 0) {
-                System.out.println("Incorrect performance log");
+            else if (curTime.compareTo(cur.stamp) >= 0) {
+                isError = true;
                 break;
             } else {
                 curTime = cur.stamp;
@@ -45,23 +49,49 @@ public class PerformanceLog {
                     && stack.peek().name.equals(cur.name)
                     && stack.peek().action.equals("START")
                     && cur.action.equals("END")) {
-                res.put(cur.name, cur.stamp.getTime() - stack.peek().stamp.getTime());
+//                res.put(cur.name, sdf.format(new Date(cur.stamp.getTime() -
+//                        stack.peek().stamp.getTime())));
+                res.put(cur.name, helper(cur.stamp.getTime() -
+                        stack.peek().stamp.getTime()));
                 stack.pop();
             } else {
+                if (res.containsKey(cur.name)) {
+                    isError = true;
+                    break;
+                }
                 stack.push(cur);
-                res.put(cur.name, 0L);
-//                res.add(cur.name + " " +
-//                        (cur.stamp.getTime() - stack.peek().stamp.getTime()));
-//                        sdf.format(new Date((cur.stamp.getTime() - stack.peek().stamp.getTime()) / 1000)));
+                res.put(cur.name, null);
             }
         }
 
-        if (!stack.isEmpty()) {
+        if (isError || !stack.isEmpty()) {
             System.out.println("Incorrect performance log");
         } else {
-            for (Map.Entry<String, Long> entry : res.entrySet()) {
+            for (Map.Entry<String, String> entry : res.entrySet()) {
                 System.out.println(entry.getKey() + " " + entry.getValue());
             }
         }
+    }
+
+    private static String helper(long l) {
+        int hour = (int) ((l / 1000) / (60 * 60));
+        int min = (int) ((l / 1000) / (60) % 60);
+        int s = (int) ((l / 1000) % 60);
+        StringBuilder sb = new StringBuilder();
+        if (hour < 10)
+            sb.append('0').append(hour).append(':');
+        else
+            sb.append(hour + ":");
+        if (min < 10)
+            sb.append('0').append(min).append(":");
+        else
+            sb.append(min + ":");
+
+        if (s < 10)
+            sb.append('0').append(s);
+        else
+            sb.append(s);
+
+        return sb.toString();
     }
 }
