51196e1721b05fe3340e97985d0cebe0df61157a
Ernest <earneyzxl@gmail.com>
2015-11-10 17:41:13 +0800

    change time format for "RddCal"

--- other/src/main/java/RddCal.java
+++ other/src/main/java/RddCal.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -8,9 +11,11 @@ import java.util.TreeMap;
  * 统计spark的log中RDD数目的变化，分析的文件是经过“grep rdd_" 后得到的文件
  * 本例中各RDD的编号为3\9\18……，行中有Added表示有rdd增加，有Removed表示有rdd被remove
  * 有Removing表示某rdd全部被删除
+ * 15/11/03 15:41:13.123 INFO BlockManagerInfo: Added rdd_30_9 in memory on 11.11.0.65:59604 (size: 285.9 MB, free: 4.3 GB)
  */
 public class RddCal {
     public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yy/MM/dd HH:mm:ss.SSS");
         Map<Integer, Integer> rddCount = new TreeMap<>();
         rddCount.put(3, 0);
         rddCount.put(9, 0);
@@ -33,12 +38,14 @@ public class RddCal {
             BufferedReader br = new BufferedReader(fileReader);
             String str;
 
-            String preTime = null, curTime;
+            Date preTime = null, curTime;
+            long curStamp = 0;
             while ((str = br.readLine()) != null) {
 
-                curTime = str.substring(9, 17);
+                curTime = sdf.parse(str.substring(0, 21));
                 if (preTime != null && !curTime.equals(preTime)) {
-                    System.out.println(preTime + " " + rddCount.values());
+                    System.out.println(sdf.format(preTime) + " " + curStamp + " " + rddCount.values());
+                    curStamp += curTime.getTime() - preTime.getTime();
                 }
                 preTime = curTime;
 
@@ -68,10 +75,10 @@ public class RddCal {
                 }
             }
             // the last time
-            System.out.println(preTime + " " + rddCount.values());
+            System.out.println(sdf.format(preTime) + " " + curStamp + " " + rddCount.values());
             fileReader.close();
             br.close();
-        } catch (IOException e) {
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
     }
