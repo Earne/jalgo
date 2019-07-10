e35ef2dbb599dba436836a3f65b289ba93d2d479
Ernest <earneyzxl@gmail.com>
2019-07-10 22:27:19 +0800

    342. Power of Four

---
+++ leetcode/src/main/java/ernest/PowerOfFour.java
@@ -0,0 +1,29 @@
+package ernest;
+
+/**
+ * 342. Power of Four
+ * Easy
+ *
+ * Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+ *
+ * Example 1:
+ *
+ * Input: 16
+ * Output: true
+ *
+ * Example 2:
+ *
+ * Input: 5
+ * Output: false
+ *
+ * Follow up: Could you solve it without loops/recursion?
+ */
+public class PowerOfFour {
+    public boolean isPowerOfFour(int num) {
+        while (num > 1 && num % 4 == 0) {
+            num = num / 4;
+        }
+
+        return num == 1;
+    }
+}
